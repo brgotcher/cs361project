6953a9714a89edf343033ff230b47843b74e3034
brgotcher <brgotcher@gmail.com>
2021-08-13 06:12:55 -0500

    wad

--- anagramTest.js
+++ anagramTest.js
@@ -1,7 +1,7 @@
 let request = new XMLHttpRequest();
 request.open("GET", "http://flip3.engr.oregonstate.edu:3480/search/street", false);
 request.send(null);
-response = JSON.parse(request.responseText);
+response = request.responseText;
 console.log(response);
 
 phrase = document.getElementById("phrase");
