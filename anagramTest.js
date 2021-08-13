11afbf9c05f328cd00bb8b0c1cd7da3881768d99
brgotcher <brgotcher@gmail.com>
2021-08-13 01:21:38 -0500

    attempting anagram test

--- anagramTest.js
+++ anagramTest.js
@@ -1,8 +1,17 @@
-let request = new XMLHttpRequest();
-request.open("GET", "http://flip3.engr.oregonstate.edu:3480/search/street", false);
-request.send(null);
-response = JSON.parse(request.responseText);
-console.log(response);
+// let request = new XMLHttpRequest();
+// request.open("GET", "http://flip3.engr.oregonstate.edu:3480/search/street", false);
+// request.send(null);
+// response = JSON.parse(request.responseText);
+// console.log(response);
 
-phrase = document.getElementById("phrase");
-phrase.textContent = response;
+// phrase = document.getElementById("phrase");
+// phrase.textContent = response;
+
+const axois = require('axios');
+const cheerio = require('cheerio');
+
+axois.get('http://flip3.engr.oregonstate.edu:3480/search/street').then(response => {
+    const html = response.data;
+})
+
+console.log(html)
