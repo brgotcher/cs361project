dd90e92bc91bb7727be7b725d5883780ce994b28
brgotcher <brgotcher@gmail.com>
2021-08-13 06:10:27 -0500

    anagram stuff again

--- anagramTest.js
+++ anagramTest.js
@@ -1,24 +1,22 @@
-// let request = new XMLHttpRequest();
-// request.open("GET", "http://flip3.engr.oregonstate.edu:3480/search/street", false);
-// request.send(null);
-// response = JSON.parse(request.responseText);
-// console.log(response);
-
-// phrase = document.getElementById("phrase");
-// phrase.textContent = response;
-
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-let anagram = "";
-
-let str = "street"
-axios.get('http://flip3.engr.oregonstate.edu:3480/search/' + str).then((response) => {
-    anagram = response.data;
-    console.log(html);
-}).catch((error) => {
-    console.log(error);
-});
+let request = new XMLHttpRequest();
+request.open("GET", "http://flip3.engr.oregonstate.edu:3480/search/street", false);
+request.send(null);
+response = JSON.parse(request.responseText);
+console.log(response);
 
 phrase = document.getElementById("phrase");
-phrase.textContent = anagram;
+phrase.textContent = response;
+
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+
+// let anagram = "";
+
+// let str = "street"
+// axios.get('http://flip3.engr.oregonstate.edu:3480/search/' + str).then((response) => {
+//     anagram = response.data;
+//     console.log(html);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
