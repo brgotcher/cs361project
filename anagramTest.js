28b1b2087ff0f7087d0e04dfaef9e6dd4cb2ddac
brgotcher <brgotcher@gmail.com>
2021-08-13 01:31:11 -0500

    more tests

--- anagramTest.js
+++ anagramTest.js
@@ -10,7 +10,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-axios.get('http://flip3.engr.oregonstate.edu:3480/search/street').then((response) => {
+axios.get('http://flip3.engr.oregonstate.edu:3480/search/hhiugyuhgu').then((response) => {
     const html = response.data;
     console.log(html);
 }).catch((error) => {
